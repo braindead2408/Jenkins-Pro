1470a3c26d396d9c77ace929ddb5d2dddcfb4187
Tanmay Nagepatil <brainsniper2408@gmail.com>
2023-06-21 00:22:08 +0530

    Update installjenkins-for-ubuntu.sh

--- installjenkins-for-ubuntu.sh
+++ installjenkins-for-ubuntu.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt install openjdk-11-jre -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
